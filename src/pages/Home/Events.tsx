b5161a7904c9d9904a95bd2d205524c313fa3ac6
PJAheibam <prosenjitsingha28@gmail.com>
2023-01-22 13:13:52 +0600

    event list designed

--- src/pages/Home/Events.tsx
+++ src/pages/Home/Events.tsx
@@ -1,20 +1,40 @@
-import { Paper, Box, Typography, IconButton } from "@mui/material";
+import {
+  Paper,
+  Box,
+  Typography,
+  IconButton,
+  List,
+  ListItem,
+  Grid,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
 import TuneRoundedIcon from "@mui/icons-material/TuneRounded";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Event } from "../../types/event.type";
 
+import ClockIcon from "@mui/icons-material/AccessTimeRounded";
+import CalendarIcon from "@mui/icons-material/CalendarMonthRounded";
+
 function Events() {
   const { data = [], isLoading } = useQuery({
     queryKey: ["events"],
     queryFn: async (): Promise<Event[]> =>
-      axios.get(
-        "https://secquraise-pj-default-rtdb.firebaseio.com/events.json"
-      ),
+      axios
+        .get("https://secquraise-pj-default-rtdb.firebaseio.com/events.json")
+        .then((res) => res.data),
   });
+
+  const handleEventClick = () => {};
   console.info(data);
   return (
-    <Paper elevation={3} sx={{ width: "30%", m: 1, mr: [2, 3], p: [1, 2] }}>
+    <Paper
+      elevation={3}
+      sx={{ width: "40%", minWidth: "355px", m: 1, mr: [2, 3], p: [1, 2] }}
+    >
+      {/* Header */}
       <Box
         sx={{
           display: "flex",
@@ -29,6 +49,44 @@ function Events() {
           <TuneRoundedIcon />
         </IconButton>
       </Box>
+      {/* List */}
+
+      <List>
+        {data.map((event) => (
+          <ListItemButton
+            component={Grid}
+            container
+            sx={(theme) => ({
+              p: 1,
+              borderRadius: 2,
+              ":hover": {
+                outline: `1px solid ${theme.palette.divider}`,
+              },
+            })}
+          >
+            <Grid item xs={7}>
+              <Typography>{`${event.ID}: ${event.Location}`}</Typography>
+              <Typography>Person Detected!</Typography>
+            </Grid>
+            <Grid component={List} disablePadding item xs={5}>
+              {/* time */}
+              <ListItem disablePadding>
+                <ListItemIcon sx={{ minWidth: 30 }}>
+                  <ClockIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>{event.Time}</ListItemText>
+              </ListItem>
+              {/* date */}
+              <ListItem disablePadding>
+                <ListItemIcon sx={{ minWidth: 30 }}>
+                  <CalendarIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>{event.Date}</ListItemText>
+              </ListItem>
+            </Grid>
+          </ListItemButton>
+        ))}
+      </List>
     </Paper>
   );
 }
